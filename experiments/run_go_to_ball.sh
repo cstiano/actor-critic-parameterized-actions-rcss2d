1e54dd93369f34448aa5959e6c1281c4f3e0d4d4
Cristiano Oliveira <cso@cin.ufpe.br>
2020-08-23 17:33:34 -0300

    shell go to ball

---
+++ experiments/run_go_to_ball.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Configure the Python Path to use in the project
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PYTHONPATH=$PYTHONPATH:$DIR/..
+
+# Run HFO
+HFO/bin/HFO --fullstate --no-logging --headless --offense-agents=1 --defense-npcs=1 --offense-team=$1 --defense-team=$2 --trials $3 &
+sleep 5
+# Sleep is needed to make sure doesn't get connected too soon, as unum 1 (goalie)
+python ./src/train/go_to_ball_ddpg.py &
+echo "conectar"
+sleep 4
+
+trap "kill -TERM -$$" SIGINT
+wait
