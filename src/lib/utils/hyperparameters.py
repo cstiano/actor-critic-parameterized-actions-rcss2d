7f0aeccc1c5f0116a75ecf1b76383d8ea784ff8e
Cristiano Oliveira <cso@cin.ufpe.br>
2020-08-30 14:15:33 -0300

    buffer param

--- src/lib/utils/hyperparameters.py
+++ src/lib/utils/hyperparameters.py
@@ -48,7 +48,7 @@ PARAMS = {
     'clip_param': 0.2,
     'max_grad_norm' : 0.5,
     'ppo_update_time': 10,
-    'buffer_capacity': 1000000,
+    'buffer_capacity': 1000,
     'batch_size': 32,
     'ppo_epoch': 10
   }
