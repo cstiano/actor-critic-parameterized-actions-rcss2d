49abc84457872727f151e0d179c53569ea1a4972
Cristiano Oliveira <cso@cin.ufpe.br>
2020-09-03 19:51:09 -0300

    feat(paper reward)

--- src/lib/utils/reward_selector.py
+++ src/lib/utils/reward_selector.py
@@ -1,8 +1,10 @@
 from src.lib.utils.state_wrapper import StateWrapper
+import hfo 
 
 TEST_REWARD = 0
 GO_TO_BALL_REWARD = 1
 BALL_PROXIMITY_GOAL_REWARD = 2
+PAPER_REWARD = 3
 
 MAX_DISTANCE = 50.0
 MIN_DISTANCE_TO_BALL = 2.0
@@ -30,21 +32,41 @@ class RewardSelector:
     def get_reward_go_to_ball(self, act, state_wrapper, done, status):
         distance_to_ball = state_wrapper.get_distance_to_ball()
         reward = (MAX_DISTANCE - distance_to_ball) / MAX_DISTANCE
-        if distance_to_ball <= 2.0:
-            return 1.0
         if distance_to_ball > self.last_distance_to_ball:
             reward = (-1.0) * (1.0 - reward)
         self.last_distance_to_ball = distance_to_ball
+        if distance_to_ball <= 2.0:
+            return 1.0
         return reward
     
     def get_reward_ball_proximity_goal(self, act, state_wrapper, done, status):
         ball_distance_to_goal = state_wrapper.get_ball_distance_to_goal()
         reward = (MAX_BALL_DISTANCE_TO_GOAL - ball_distance_to_goal) / MAX_BALL_DISTANCE_TO_GOAL
+        if ball_distance_to_goal > self.last_distance_to_ball:
+            reward = (-1.0) * (1.0 - reward)
+        self.last_ball_distance_to_goal = ball_distance_to_goal
         if ball_distance_to_goal > 30.0:
             return -1
         if ball_distance_to_goal <= 2.0:
             return 1.0
-        if ball_distance_to_goal > self.last_distance_to_ball:
-            reward = (-1.0) * (1.0 - reward)
+        return reward
+    
+    def get_reward_paper(self, act, state_wrapper, done, status):
+        distance_to_ball = state_wrapper.get_distance_to_ball()
+        ball_distance_to_goal = state_wrapper.get_ball_distance_to_goal()
+        i_kick = 0.0
+        i_goal = 0.0
+
+        if state_wrapper.is_able_to_kick():
+            i_kick = 1.0
+        if status == hfo.GOAL:
+            i_goal = 5.0
+        
+        r_dist_ball = self.last_distance_to_ball - distance_to_ball
+        r_dist_goal = self.last_ball_distance_to_goal - ball_distance_to_goal
+
+        reward = r_dist_ball + i_kick + (3.0 * r_dist_goal) + i_goal
+
+        self.last_distance_to_ball = distance_to_ball
         self.last_ball_distance_to_goal = ball_distance_to_goal
         return reward
