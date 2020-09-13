feb4c32ab58e3cde1c199dd894e0c5978aa2d63e
Cristiano Oliveira <cso@cin.ufpe.br>
2020-09-13 16:59:41 -0300

    feat(environment type): controling the environment type with the server flag

--- shell/run_agent_train.sh
+++ shell/run_agent_train.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PYTHONPATH=$PYTHONPATH:$DIR/..
 
 # Run HFO
-HFO/bin/HFO --fullstate --no-logging --untouched-time=$3 --headless --offense-agents=1 --defense-npcs=1 --offense-team=helios --defense-team=helios --trials $2 &
+HFO/bin/HFO --fullstate --no-logging --untouched-time=$3 --env-type=$4 --headless --offense-agents=1 --defense-npcs=1 --offense-team=helios --defense-team=helios --trials $2 &
 sleep 5
 # Sleep is needed to make sure doesn't get connected too soon, as unum 1 (goalie)
 python ./src/agent/$1.py &
