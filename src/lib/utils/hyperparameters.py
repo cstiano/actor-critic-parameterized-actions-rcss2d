03a629487a6a8b49a002320bbc128de9a472ff56
Cristiano Oliveira <cso@cin.ufpe.br>
2020-09-13 21:01:02 -0300

    feat(hyperparameters): included random steps to params

--- src/lib/utils/hyperparameters.py
+++ src/lib/utils/hyperparameters.py
@@ -8,7 +8,8 @@ PARAMS = {
     'soft_tau': 1e-2,
     'batch_size': 64,
     'saving_cycle': 200,
-    'noise_factor': 0.1
+    'noise_factor': 0.1,
+    'random_steps': 1000
   },
   'sac':{
     'replay_buffer_size': 1000000,
@@ -22,7 +23,8 @@ PARAMS = {
     'saving_cycle': 200,
     'mean_lambda':1e-3,
     'std_lambda':1e-3,
-    'z_lambda':0.0
+    'z_lambda':0.0,
+    'random_steps': 1000
   },
   'td3':{
     'replay_buffer_size': 1000000,
@@ -35,7 +37,8 @@ PARAMS = {
     'saving_cycle': 200,
     'noise_std': 0.2,
     'noise_clip': 0.5,
-    'policy_update': 2
+    'policy_update': 2,
+    'random_steps': 1000
   },
   'ppo':{
     'hidden_dim': 256,
@@ -50,6 +53,7 @@ PARAMS = {
     'ppo_update_time': 10,
     'buffer_capacity': 1000,
     'batch_size': 32,
-    'ppo_epoch': 10
+    'ppo_epoch': 10,
+    'random_steps': 1000
   }
 }
