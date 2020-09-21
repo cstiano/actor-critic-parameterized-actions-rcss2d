80b3f10c98f0fc678fc4223d896e31b5f7edc61f
Cristiano Oliveira <cso@cin.ufpe.br>
2020-09-21 20:51:26 -0300

    feat(ball to goal): ddpg and td3

--- src/agent/ball_to_goal/ball_to_goal_ddpg.py
+++ src/agent/ball_to_goal/ball_to_goal_ddpg.py
@@ -16,10 +16,10 @@ from src.lib.utils.reward_selector import *
 from src.lib.utils.action_selector import CONDITIONAL_DASH_OR_KICK_ACTION
 from src.lib.utils.reward_selector import AGENT_AND_BALL_POTENCIAL_REWARD
 from src.lib.utils.state_selector import AGENT_ORIENTATION_AND_BALL_POSITION_SPACE
+from src.lib.utils.ounoise import OUNoise
 from src.lib.utils.hyperparameters import PARAMS
-from src.actor_critic_arch.baseline_rlad_sac import SAC
+from src.actor_critic_arch.baseline_rlad_ddpg import DDPG
 from src.lib.utils.replay_buffer import *
-
 
 parse = argparse.ArgumentParser(
     description='Agent Args', formatter_class=argparse.RawTextHelpFormatter)
@@ -27,12 +27,10 @@ parse.add_argument('--play', dest='play', action='store_true',
                    default=False, help='Agent Playing.')
 args = parse.parse_args()
 
-args = parse.parse_args()
 TEAM = 'HELIOS'
 PORT = 6000
-ACTOR_MODEL_NAME = "sac_actor_ball_to_goal"
-CRITIC_MODEL_NAME = "sac_critic_ball_to_goal"
-SOFT_MODEL_NAME = "sac_soft_ball_to_goal"
+ACTOR_MODEL_NAME = "ddpg_actor_ball_to_goal"
+CRITIC_MODEL_NAME = "ddpg_critic_ball_to_goal"
 ENABLE_LOSS_WRITE = False
 
 hfo_env = HFOEnv(is_offensive=True, strict=True,
@@ -41,35 +39,39 @@ hfo_env = HFOEnv(is_offensive=True, strict=True,
                  selected_reward=AGENT_AND_BALL_POTENCIAL_REWARD,
                  selected_state=AGENT_ORIENTATION_AND_BALL_POSITION_SPACE)
 unum = hfo_env.getUnum()
-params = PARAMS['sac']
-sac = SAC(hfo_env.observation_space.shape[0],
-          hfo_env.action_space.shape[0], params)
+params = PARAMS['ddpg']
+ddpg = DDPG(
+    hfo_env.observation_space.shape[0], hfo_env.action_space.shape[0], params)
+ou_noise = OUNoise(hfo_env.action_space)
 replay_buffer = ReplayBuffer(params['replay_buffer_size'])
+
 
 def train():
     writer = SummaryWriter(
-        'logs/{}_SAC_BALL_TO_GOAL'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
+        'logs/{}_DDPG_BALL_TO_GOAL'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
     frame_idx = 0
-
+    
     try:
         for episode in itertools.count():
             status = hfo.IN_GAME
             done = True
             state = hfo_env.reset()
-            episode_reward = 0.0
+            episode_reward = 0
             step = 0
 
             while status == hfo.IN_GAME:
-                action = sac.policy_network.get_action(state)
+                action = ddpg.policy_network.get_action(state)
+                action = ou_noise.get_action(action, step)
+
                 next_state, reward, done, status = hfo_env.step(action)
 
-                replay_buffer.push(state, action, reward, next_state, done)
+                replay_buffer.push(
+                    state, action, reward, next_state, done)
 
                 if len(replay_buffer) > params['batch_size']:
-                    q_value_loss, value_loss, policy_loss = sac.soft_q_update(replay_buffer)
-                    
+                    value_loss, policy_loss = ddpg.ddpg_update(replay_buffer)
+
                     if ENABLE_LOSS_WRITE:
-                        writer.add_scalar(f'Q_Value_Loss', q_value_loss, frame_idx)
                         writer.add_scalar(f'Value_Loss', value_loss, frame_idx)
                         writer.add_scalar(f'Policy_Loss', policy_loss, frame_idx)
                         frame_idx += 1 
@@ -82,8 +84,7 @@ def train():
                     break
 
             if (episode % params['saving_cycle']) == 0:
-                sac.save_model(ACTOR_MODEL_NAME,
-                               CRITIC_MODEL_NAME, SOFT_MODEL_NAME)
+                ddpg.save_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME)
             writer.add_scalar(
                 f'Rewards/epi_reward', episode_reward, global_step=episode)
 
@@ -92,23 +93,24 @@ def train():
                 writer.close()
                 exit()
     except KeyboardInterrupt:
-        sac.save_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME, SOFT_MODEL_NAME)
+        ddpg.save_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME)
         writer.close()
 
 
 def play():
-    sac.load_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME, SOFT_MODEL_NAME)
+    ddpg.load_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME)
     for episode in itertools.count():
         status = hfo.IN_GAME
         done = True
-        state = hfo_env.get_state()
+        state = hfo_env.reset()
 
         while status == hfo.IN_GAME:
-            action = sac.policy_network.get_action(state)
+            action = ddpg.policy_network.get_action(state)
             action = action.astype(np.float32)
             next_state, reward, done, status = hfo_env.step([action])
 
             state = next_state
+
             if done:
                 break
         if status == hfo.SERVER_DOWN:
--- src/agent/ball_to_goal/ball_to_goal_td3.py
+++ src/agent/ball_to_goal/ball_to_goal_td3.py
@@ -17,9 +17,10 @@ from src.lib.utils.action_selector import CONDITIONAL_DASH_OR_KICK_ACTION
 from src.lib.utils.reward_selector import AGENT_AND_BALL_POTENCIAL_REWARD
 from src.lib.utils.state_selector import AGENT_ORIENTATION_AND_BALL_POSITION_SPACE
 from src.lib.utils.hyperparameters import PARAMS
-from src.actor_critic_arch.baseline_rlad_sac import SAC
+from src.lib.utils.gaussian_exploration import GaussianExploration
+from src.lib.utils.ounoise import OUNoise
+from src.actor_critic_arch.baseline_rlad_td3 import TD3
 from src.lib.utils.replay_buffer import *
-
 
 parse = argparse.ArgumentParser(
     description='Agent Args', formatter_class=argparse.RawTextHelpFormatter)
@@ -27,12 +28,10 @@ parse.add_argument('--play', dest='play', action='store_true',
                    default=False, help='Agent Playing.')
 args = parse.parse_args()
 
-args = parse.parse_args()
 TEAM = 'HELIOS'
 PORT = 6000
-ACTOR_MODEL_NAME = "sac_actor_ball_to_goal"
-CRITIC_MODEL_NAME = "sac_critic_ball_to_goal"
-SOFT_MODEL_NAME = "sac_soft_ball_to_goal"
+ACTOR_MODEL_NAME = "td3_actor_ball_to_goal"
+CRITIC_MODEL_NAME = "td3_critic_ball_to_goal"
 ENABLE_LOSS_WRITE = False
 
 hfo_env = HFOEnv(is_offensive=True, strict=True,
@@ -41,14 +40,16 @@ hfo_env = HFOEnv(is_offensive=True, strict=True,
                  selected_reward=AGENT_AND_BALL_POTENCIAL_REWARD,
                  selected_state=AGENT_ORIENTATION_AND_BALL_POSITION_SPACE)
 unum = hfo_env.getUnum()
-params = PARAMS['sac']
-sac = SAC(hfo_env.observation_space.shape[0],
-          hfo_env.action_space.shape[0], params)
+params = PARAMS['td3']
+td3 = TD3(
+    hfo_env.observation_space.shape[0], hfo_env.action_space.shape[0], params)
 replay_buffer = ReplayBuffer(params['replay_buffer_size'])
+
 
 def train():
     writer = SummaryWriter(
-        'logs/{}_SAC_BALL_TO_GOAL'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
+        'logs/{}_TD3_BALL_TO_GOAL'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")))
+    noise = OUNoise(hfo_env.action_space)
     frame_idx = 0
 
     try:
@@ -56,21 +57,23 @@ def train():
             status = hfo.IN_GAME
             done = True
             state = hfo_env.reset()
-            episode_reward = 0.0
+            episode_reward = 0
             step = 0
 
             while status == hfo.IN_GAME:
-                action = sac.policy_network.get_action(state)
+                action = td3.policy_network.get_action(state)
+                action = noise.get_action(action, step)
                 next_state, reward, done, status = hfo_env.step(action)
 
-                replay_buffer.push(state, action, reward, next_state, done)
+                replay_buffer.push(
+                    state, action, reward, next_state, done)
 
                 if len(replay_buffer) > params['batch_size']:
-                    q_value_loss, value_loss, policy_loss = sac.soft_q_update(replay_buffer)
-                    
+                    value_loss1, value_loss2, policy_loss = td3.td3_update(replay_buffer, step)
+
                     if ENABLE_LOSS_WRITE:
-                        writer.add_scalar(f'Q_Value_Loss', q_value_loss, frame_idx)
-                        writer.add_scalar(f'Value_Loss', value_loss, frame_idx)
+                        writer.add_scalar(f'Value_Loss1', value_loss1, frame_idx)
+                        writer.add_scalar(f'Value_Loss2', value_loss2, frame_idx)
                         writer.add_scalar(f'Policy_Loss', policy_loss, frame_idx)
                         frame_idx += 1 
 
@@ -82,8 +85,7 @@ def train():
                     break
 
             if (episode % params['saving_cycle']) == 0:
-                sac.save_model(ACTOR_MODEL_NAME,
-                               CRITIC_MODEL_NAME, SOFT_MODEL_NAME)
+                td3.save_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME)
             writer.add_scalar(
                 f'Rewards/epi_reward', episode_reward, global_step=episode)
 
@@ -92,28 +94,26 @@ def train():
                 writer.close()
                 exit()
     except KeyboardInterrupt:
-        sac.save_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME, SOFT_MODEL_NAME)
+        td3.save_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME)
         writer.close()
 
 
 def play():
-    sac.load_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME, SOFT_MODEL_NAME)
+    td3.load_model(ACTOR_MODEL_NAME, CRITIC_MODEL_NAME)
     for episode in itertools.count():
         status = hfo.IN_GAME
         done = True
         state = hfo_env.get_state()
 
         while status == hfo.IN_GAME:
-            action = sac.policy_network.get_action(state)
+            action = td3.policy_network.get_action(state)
             action = action.astype(np.float32)
             next_state, reward, done, status = hfo_env.step([action])
 
             state = next_state
+
             if done:
                 break
-        if status == hfo.SERVER_DOWN:
-            hfo_env.act(hfo.QUIT)
-            exit()
 
 
 if __name__ == '__main__':
